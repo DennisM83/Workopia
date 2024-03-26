266ce1461e226fc24c9ae4002d68fcade8f53cd3
Dennis Wenrich <dennisfinley@gmail.com>
2024-03-25 23:25:09 -0400

    implemented a loadView and loadPartial function for code organization

--- helpers.php
+++ helpers.php
@@ -10,3 +10,24 @@
  function basePath($path = '') {
     return __DIR__ . '/' . $path;
  }
+
+ /**
+  * Loading views
+  *
+  * @param string $name
+  * @return void
+  */
+
+  function loadView($name) {
+    require basePath("views/{$name}.views.php");
+  }
+
+  /**
+   * Loading partials
+   * 
+  * @param string $name
+  * @return void
+  */
+  function loadPartial($name) {
+    require "views/partials/{$name}.php";
+  }
