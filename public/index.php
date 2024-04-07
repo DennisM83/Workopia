bb69eda6ccf3a55ecd8600a213e5a16bfdfc78a7
Dennis Wenrich <dennisfinley@gmail.com>
2024-04-07 15:41:02 -0400

    refactored index file and added proper comments

--- public/index.php
+++ public/index.php
@@ -1,18 +1,18 @@
 <?php
 
     require '../helpers.php';
-    require basePath('Database.php');
-    $config = require basePath('config/db.php');
-
-    $db = new Database($config);
-
     require basePath('Router.php');
+    require basePath('Database.php');
 
+    // Instatiate the router
     $router = new Router();
 
+    // Get routes
     $routes = require basePath('routes.php');
 
+    //Get current URI and Request Method
     $uri = $_SERVER['REQUEST_URI'];
     $method = $_SERVER['REQUEST_METHOD'];
 
+    // Route the request
     $router->route($uri, $method);
