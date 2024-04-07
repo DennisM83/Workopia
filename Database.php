10ed9c8fb2be81fef78e13f889112c3ab072aafe
Dennis Wenrich <dennisfinley@gmail.com>
2024-04-07 15:38:44 -0400

    Set default PDO fetch mode to FETCH_OBJ for improved consistency and readability

--- Database.php
+++ Database.php
@@ -15,14 +15,30 @@ class Database {
 
         $option = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
         ];
 
         try {
             $this->conn = new PDO($dsn, $config['username'], $config['password'], $option);
-            echo 'Database connection established';
         } catch (PDOException $e){
             throw new Exception("Database connection failed: {$e->getMessage()}");
+        }
+    }
+
+    /**
+     * Get the query
+     *
+     * @param string $query
+     * @return PDOStatement
+     * @throws PDOException
+     */
+    public function query($query) {
+        try {
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            return $stmt;
+        } catch (PDOException $e) {
+            throw new Exception("Query failed to execute: {$e->getMessage()}");
         }
     }
 }
