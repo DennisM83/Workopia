abfd5fca9215927cad29e57c2ad38ec33b2d14a6
Dennis Wenrich <dennisfinley@gmail.com>
2024-04-13 09:41:22 -0400

    Update listings index to fetch data from database

--- controllers/listings/index.php
+++ controllers/listings/index.php
@@ -1,3 +1,10 @@
 <?php
 
-loadView('listings/index');
+$config = require basePath('config/db.php');
+$db = new Database($config);
+
+$listings = $db->query('SELECT * FROM listings')->fetchAll();
+
+loadview('listings/index', [
+    'listings' => $listings,
+]);
