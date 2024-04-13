45f0b080deaa96a23a32acda0bdce7246d512e9c
Dennis Wenrich <dennisfinley@gmail.com>
2024-04-13 13:51:12 -0400

    Moved files to App directory

---
+++ App/controllers/listings/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = require basePath('config/db.php');
+$db = new Database($config);
+
+$listings = $db->query('SELECT * FROM listings')->fetchAll();
+
+loadview('listings/index', [
+    'listings' => $listings,
+]);
