fb79ff29e408fc03e1f855b0e001292a25f931bb
Dennis Wenrich <dennisfinley@gmail.com>
2024-05-04 10:03:57 -0400

    Type declaration for $message parameter in notFound method

--- App/controllers/ErrorController.php
+++ App/controllers/ErrorController.php
@@ -10,7 +10,7 @@ class ErrorController {
      * @param string $message
      * @return void
      */
-    public static function notFound($message = 'Resource not found') {
+    public static function notFound(string $message = 'Resource not found') {
         http_response_code(404);
 
         loadView('error', [
@@ -25,7 +25,7 @@ class ErrorController {
      * @param string $message
      * @return void
      */
-    public static function unauthorized($message = 'You are not authorized to view this resource') {
+    public static function unauthorized(string $message = 'You are not authorized to view this resource') {
         http_response_code(403);
 
         loadView('error', [
